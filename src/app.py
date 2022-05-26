5947ab4751706a635d9fb44aacc061d40fb84473
jorislimonier <joris.limonier@hotmail.fr>
2022-05-26 08:35:01 +0200

    Deploy heroku app that didn't deploy properly

--- src/app.py
+++ src/app.py
@@ -5,6 +5,7 @@ from dash.dependencies import Input, Output
 
 from color_data import ColorData
 import utils
+# import dev_hints
 
 BG_COLOR = "white"
 
@@ -115,39 +116,6 @@ def store_lastname_digits(ln):
     return None
 
 
-# Display
-## Birthdate digit
-@app.callback(
-    Output("birthdate_digit", "children"),
-    Input("birthdate_digit_store", "data"),
-)
-def display_birthdate_digit(birthdate_digit):
-    """Takes a birth date and returns its color digit"""
-    if birthdate_digit is not None:
-        return f"Your birthdate digit is: {birthdate_digit}"
-    return None
-
-
-## First name digit
-@app.callback(
-    Output("firstname_digits", "children"),
-    Input("firstname_digits_store", "data"),
-)
-def display_firstname_digit(firstname_digit):
-    """Takes a full name and returns its color digit"""
-    if firstname_digit is not None:
-        return f"Your first name digits are: {firstname_digit}"
-
-
-## Last name digit
-@app.callback(
-    Output("lastname_digits", "children"),
-    Input("lastname_digits_store", "data"),
-)
-def display_lastname_digits(lastname_digits):
-    """Takes a full name and returns its color digit"""
-    if lastname_digits is not None:
-        return f"Your last name digits are: {lastname_digits}"
 
 
 @app.callback(
