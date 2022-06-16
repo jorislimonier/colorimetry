81b5c75db4c00732911a34def7e251d9621c8255
jorislimonier <joris.limonier@hotmail.fr>
2022-06-16 05:46:53 +0200

    Add color path description

--- src/_data_writer.py
+++ src/_data_writer.py
@@ -1,33 +1,64 @@
 import pandas as pd
+import yaml
 
 df = pd.read_excel("data/colors_meaning.xlsx", index_col=0)
 
 with open("data/color_explanation.txt", "r") as f:
     txt = f.read()
 
-
-clr_data = [[line for line in par.split("\n")] for par in txt.split("\n\n")]
+with open("data/color_description.yaml", "r") as stream:
+    color_descr: dict = yaml.safe_load(stream=stream)
 
 
 arch1_titles = []
 arch1_descriptions = []
 arch2_titles = []
 arch2_descriptions = []
+keywords_list = []
+description_list = []
 
-for par in clr_data:
-    color_title, arch1, arch2, kw = par
-    arch1_title, arch1_description = arch1.split(" - ")
-    arch2_title, arch2_description = arch2.split(" - ")
+colors = [*color_descr]
 
-    arch1_titles.append(arch1_title)
-    arch1_descriptions.append(arch1_description)
-    arch2_titles.append(arch2_title)
-    arch2_descriptions.append(arch2_description)
+for color in colors:
+    archetype1_title, archetype1_description = color_descr[color]["archetype1"].values()
+    archetype2_title, archetype2_description = color_descr[color]["archetype2"].values()
+
+    keywords = color_descr[color]["keywords"]
+    description = color_descr[color]["description"]
+
+    arch1_titles.append(archetype1_title)
+    arch1_descriptions.append(archetype1_description)
+    arch2_titles.append(archetype2_title)
+    arch2_descriptions.append(archetype2_description)
+    keywords_list.append(keywords)
+    description_list.append(description)
+    # df[df["color"] == color][color_attributes]
+print(df)
 
 df["archetype1_title"] = arch1_titles
 df["archetype1_description"] = arch1_descriptions
 df["archetype2_title"] = arch2_titles
 df["archetype2_description"] = arch2_descriptions
+df["keywords"] = keywords_list
+df["description"] = description_list
 
-if False:
+print(df)
+
+# clr_data = [[line for line in par.split("\n")] for par in txt.split("\n\n")]
+
+
+
+
+# for par in clr_data:
+#     color_title, arch1, arch2, kw = par
+#     arch1_title, arch1_description = arch1.split(" - ")
+#     arch2_title, arch2_description = arch2.split(" - ")
+
+#     arch1_titles.append(arch1_title)
+#     arch1_descriptions.append(arch1_description)
+#     arch2_titles.append(arch2_title)
+#     arch2_descriptions.append(arch2_description)
+
+
+if True:
     df.to_excel("data/colors_meaning.xlsx")
--- src/pages/colors/colors.py
+++ src/pages/colors/colors.py
@@ -21,23 +21,48 @@ for color in COLOR_LIST:
     rows = []
     for row_idx in range(1, 3):
         archetype_title = dbc.Col(
-            html.H3(color_data[f"archetype{row_idx}_title"].capitalize()),
-            sm=title_width["sm"],
-            md=title_width["md"],
-            lg=title_width["lg"],
+            html.H4(color_data[f"archetype{row_idx}_title"].capitalize()),
+            sm=5,
+            md=5,
+            lg={"size": 4, "offset": 1},
         )
 
         archetype_descr = dbc.Col(
             html.H6(color_data[f"archetype{row_idx}_description"]),
-            sm=descr_width["sm"],
-            md=descr_width["md"],
-            lg=descr_width["lg"],
+            sm=9,
+            md=11,
+            lg={"size": 6},
         )
 
-        rows.append(dbc.Row(children=[archetype_title, archetype_descr]))
-    
-    rows.insert(1, html.Br())
-    
+        rows.append(
+            dbc.Row(
+                children=[archetype_title, archetype_descr],
+                style={"margin-top": "30px"},
+            )
+        )
+
+    rows.append(
+        dbc.Row(
+            dbc.Col(
+                children=html.H3("Description du chemin de vie"),
+                style={"text-align": "center", "margin-top": "30px"},
+            ),
+        )
+    )
+    rows.append(
+        dbc.Row(
+            dbc.Col(
+                color_data["description"],
+                width={"size": 10, "offset": 1},
+            ),
+            style={"margin-top": "20px"},
+        ),
+    )
+
+    # rows.insert(3, html.Br())
+    # rows.insert(2, html.Br())
+    # rows.insert(1, html.Br())
+
     color_layout[color] = html.Div(
         children=[title, *rows],
         style={"margin": " 20px"},
